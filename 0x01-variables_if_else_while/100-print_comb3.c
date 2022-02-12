d9bb575d4fe896eae26ade02b6fab2a2ac564652
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-11 23:21:47 -0500

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 /**
-* main - Print the numbers of base 16 in lowercase
-*
-* Return: Always 0 (Success)
-*/
+ * main - Print the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int tens;
-  		int ones;
+	int tens;
+	int ones;
 
-		for (tens = 0; tens <= 9; tens++)
-    	{
-    	  for (ones = tens + 1; ones <= 9; ones++)
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
 		{
-		putchar(tens + '0');
-		putchar(ones + '0');
-		if (tens < 8)
-		{
-	    putchar(',');
-	    putchar(' ');
-	    }
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-    	}
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }

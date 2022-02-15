c09c75f14e18722439491795d9c916d5fc82ab5e
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-15 08:42:14 -0500

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,14 @@
 
 int main(void)
 {
-    int i;
-    char p[] = "_putchar";
-
-    for (i = 0; i < 9; i++)
-        _putchar(p[i]);
-    _putchar('\n');
-    return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }

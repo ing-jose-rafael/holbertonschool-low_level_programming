e3c4d6995c6b31d11eeac5ae761d65993f706716
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-16 14:03:41 -0500

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,25 +5,27 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ * 
  * Return: largest number
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if (a >= b && a >= c)
-    {
-        largest = a;
-    }
-    else if (b >= a && b >= c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+if (a >= b && a >= c)
+{
+largest = a;
+}
+else if (b >= a && b >= c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-    return (largest);
+return (largest);
 }

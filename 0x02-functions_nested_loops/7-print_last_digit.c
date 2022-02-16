7e7af8da05bd0f016fb5c1c02178ea682133cc48
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-16 12:55:42 -0500

    main.h

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,6 +5,7 @@
  * @n: The number in question.
  *
  * Return: Value of the last digit.
+ * make: Jose 
  */
 int print_last_digit(int n)
 {
@@ -13,7 +14,11 @@ int print_last_digit(int n)
 	if (last_digit < 0)
 		last_digit *= -1;
 
-	_putchar(last_digit + '0');
+	/*
+	
+_putchar(last_digit + '0');
+	*/
+	_putchar(last_digit);
 
 	return (last_digit);
 }

12b3eb92b0064d33a993e284920267b3480f18e4
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-17 08:27:51 -0500

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * add_space - print spaces
+ * @space: int lines
+ * 
+ * Return: void 0 (Success)
+ */
+void add_space(int space)
+{
+if (space > 0)
+{
+_putchar(' ');
+add_space(--space);
+}
+}
+/**
+ * print_diagonal - Entry point
+ * @n: int
+ * 
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+const int TOP = n;
+
+if (n <= 0)
+	_putchar('\n');
+else
+for (n = 0; n < TOP; n++)
+{
+add_space(n);
+_putchar('\\');
+_putchar('\n');
+}
+}

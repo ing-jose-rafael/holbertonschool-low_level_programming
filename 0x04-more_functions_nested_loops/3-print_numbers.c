d00b058c378eb022749cb2c00891a04cef424a5d
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-17 08:02:08 -0500

    print number 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - Write a function that prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+    char number = '/';
+    while ((number++) < '9')
+        _putchar(number);
+}

b8f5716343ca7a514b6d11b84adddc115cd7351a
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-17 21:25:46 -0500

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,27 +8,27 @@
 
 void print_number(int n)
 {
-	int length = 10;
-	int number = n;
+int length = 10;
+int number = n;
 
-	if (n < 0)
-		number = (-n);
+if (n < 0)
+number = (-n);
 
-	if (n == 0)
-		_putchar('0');
-	else
-	{
-		if (n < 0)
-		{
-			_putchar('-');
-			n = (-n);
-		}
-		for (; n > 0; n /= 100)
-			length *= 10;
-		for (n = length; n > 0; n /= 10)
-		{
-			if (!(n == length && ((number / n) % 10) == 0))
-				_putchar('0' + (number / n) % 10);
-		}
-	}
+if (n == 0)
+_putchar('0');
+else
+{
+if (n < 0)
+{
+_putchar('-');
+n = (-n);
+}
+for (; n > 0; n /= 100)
+length *= 10;
+for (n = length; n > 0; n /= 10)
+{
+if (!(n == length && ((number / n) % 10) == 0))
+_putchar('0' + (number / n) % 10);
+}
+}
 }

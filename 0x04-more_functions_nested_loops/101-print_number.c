338817c832fca161685e48868f54ef0e735a6b6b
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-17 21:11:58 -0500

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "math.h"
+/**
+* print_number - Entry point
+* @n: number
+* Return: Always 0 (Success)
+*/
+
+void print_number(int n)
+{
+	int length = 10;
+	int number = n;
+
+	if (n < 0)
+		number = (-n);
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = (-n);
+		}
+		for (; n > 0; n /= 100)
+			length *= 10;
+		for (n = length; n > 0; n /= 10)
+		{
+			if (!(n == length && ((number / n) % 10) == 0))
+				_putchar('0' + (number / n) % 10);
+		}
+	}
+
+}

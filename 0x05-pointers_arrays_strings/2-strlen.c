3616dcf4a3c3523abf6c08ecd429c5ad711856fd
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-21 11:48:44 -0500

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+* _strlen - function that returns the length of a string.
+* @s: pointer type char
+* Return: int
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length = length + 1;
+		s = s + 1;
+	}
+
+	return (length);
+}

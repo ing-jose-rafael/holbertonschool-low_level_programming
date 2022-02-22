3f064cb000e99c3c97843efa0d5737611ae4355b
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-21 21:31:10 -0500

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+/**
+* _strcpy - Entry point
+* @dest: pointer type char
+* @src: pointer type char
+* Return: Always 0 (Success)
+*/
+char *_strcpy(char *dest, char *src)
+{
+	char *first = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = *src;
+	return (first);
+}

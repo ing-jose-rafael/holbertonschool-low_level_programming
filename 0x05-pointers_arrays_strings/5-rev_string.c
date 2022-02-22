b0c578a21a4f6a55b957f117f7c3bd1580e517e3
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-21 20:13:08 -0500

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+/**
+ * rev_string - function that prints a string, in reverse,
+ *             followed by a new line
+ * @s: pointer type char
+ * Return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+char *first = s;
+int length = 0;
+char tmp;
+
+while (*s != '\0')
+s = s + 1;
+s--;
+
+while (first <= s)
+{
+tmp = *first;
+*first = *s;
+*s = tmp;
+first++;
+s--;
+}
+}

d9403df81c4410c688aefa9d02fc293f350525d8
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-24 11:13:59 -0500

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int i;
-    int len = 0;
-    
-    while (*(dest+len)!='\0')
-        len++;
-    for (i = 0; i < n && src[i]; i++)
-    {
-        dest[len + i] = src[i];
-    }
-    dest[len + i] = '\0';
-    return (dest);
+int i;
+int len = 0;
+
+while (*(dest+len)!='\0')
+    len++;
+for (i = 0; i < n && src[i]; i++)
+{
+    dest[len + i] = src[i];
+}
+dest[len + i] = '\0';
+return (dest);
 }

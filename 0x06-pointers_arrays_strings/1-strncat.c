bedf87b0dead5b737917ad29c447839c9646b1b5
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-24 11:24:41 -0500

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
  * @dest: Input string 1
  * @src: Input string 2
  * @n: Number of bytes
- * 
+ *
  * Return: Concatenated strings
  */
 
@@ -11,13 +11,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i;
 int len = 0;
-
-while (*(dest + len)!='\0')
+while (*(dest + len) != '\0')
 len++;
 for (i = 0; i < n && src[i]; i++)
 {
 dest[len + i] = src[i];
 }
 dest[len + i] = '\0';
+
 return (dest);
 }

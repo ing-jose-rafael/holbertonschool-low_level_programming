e807389c32a4c697fbfe24d9ad6b1fdf3693d363
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-24 10:30:44 -0500

    rev array

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+/**
+* cap_string - Capitalizes all words of a string..
+* @s: The pointer.
+*
+* Return: To the value of s.
+*/
+char *cap_string(char *s)
+{
+int j;
+for (j = 0; s[j] != '\0'; j++)
+{
+if (j == 0 && s[j] >= 'a' && s[j] <= 'z')
+{
+s[j] -= 32;
+}
+if ((s[j] == ' ' || s[j] == ',' || s[j] == ';' ||
+	s[j] == '.' || s[j] == '!' || s[j] == '?' ||
+	s[j] == '"' || s[j] == '(' || s[j] == ')' ||
+	s[j] == '{' || s[j] == '}' || s[j] == '\n' ||
+	s[j] == '\t') && (s[j + 1] >= 'a' && s[j + 1] <= 'z'))
+{
+s[j + 1] -= 32;
+}
+}
+return (s);
+}
